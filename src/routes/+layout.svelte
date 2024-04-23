031b7ca6365c0318bd43ee1a9b036ca9694d1879
tostu <torge.stubbe@googlemail.com>
2024-04-23 19:33:31 +0200

    landing design

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script>
-	import "../app.css";
+	import '../app.css';
 </script>
 
-<slot />
+<div class="flex min-h-svh flex-col">
+	<div class="bg-bright flex-grow dark:bg-neutral-900">
+		<slot />
+	</div>
+</div>
+
+<!--<footer class="bg-gray-800 py-4 text-white">-->
+<!--	&lt;!&ndash; Footer content goes here &ndash;&gt;-->
+<!--</footer>-->
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,20 @@
-<h1 class="text-3xl font-bold underline">
-	Hello world!
-</h1>
+<script lang="ts">
+	import type { PageData } from './$types';
+	import frog from '$lib/asset/frog.webp';
 
+	export let data: PageData;
+</script>
+
+<div class="container m-auto flex h-svh flex-col items-center justify-center">
+	<div class="space-y-5 text-center text-neutral-700 dark:text-white">
+		<h1 class="font-yamafont text-5xl">Es Ist Mittwoch</h1>
+		<img src={frog} alt="frog" id="frog-logo" class="h-auto w-96 object-contain" />
+		<h1 class="font-yamafont text-5xl">Meine Kerle</h1>
+	</div>
+</div>
 
 <style lang="postcss">
-    :global(html) {
-        background-color: theme(colors.gray.100);
-    }
+	#frog-logo {
+		filter: drop-shadow(0 0 2em theme(colors.primary));
+	}
 </style>
