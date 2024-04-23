eb2ce3a361871649b44de4fca4446bd9444c7018
tostu <torge.stubbe@googlemail.com>
2024-04-23 19:32:23 +0200

    theme setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#D9AB73',
+				secondary: '#D9C8B4',
+				accent: '#A6785D',
+				bright: '#F2F2F2',
+				dark: '#202020'
+			},
+			fontFamily: {
+				yamafont: 'yamafont, ui-serif' // Adds a new `font-display` class
+			}
+		}
+	},
+	plugins: []
+};
