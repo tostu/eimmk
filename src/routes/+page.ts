d9c6bd1f3de233bfab3d3f178a6060fcba11e80e
tostu <torge.stubbe@googlemail.com>
2024-04-28 22:22:07 +0200

    else false

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -3,7 +3,8 @@ import type { PageLoad } from './$types';
 export const load: PageLoad = () => {
 	const dateOptions: Intl.DateTimeFormatOptions = { timeZone: 'Europe/Berlin', weekday: 'long' };
 
-	const isWednesday: boolean = new Date().toLocaleString('de-DE', dateOptions) === 'Mittwoch';
+	const isWednesday: boolean =
+		new Date().toLocaleString('de-DE', dateOptions) === 'Mittwoch' || false;
 
 	return {
 		isWednesday
